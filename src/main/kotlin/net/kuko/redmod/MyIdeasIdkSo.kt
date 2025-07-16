5fded8f57db5ef8dcae014fee12dc22371b8bb9d
jak7b <olackovic82@gmail.com>
2025-07-16 07:23:27 +0200

    Initial commit

---
+++ src/main/kotlin/net/kuko/redmod/MyIdeasIdkSo.kt
@@ -0,0 +1,15 @@
+package net.kuko.redmod
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object MyIdeasIdkSo : ModInitializer {
+    private val logger = LoggerFactory.getLogger("redmod")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
---
+++ src/main/kotlin/net/kuko/redmod/MyIdeasIdkSoDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.kuko.redmod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MyIdeasIdkSoDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
