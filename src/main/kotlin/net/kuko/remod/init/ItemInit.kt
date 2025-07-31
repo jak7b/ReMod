64acbe62c35cb90ec47e78e75a3cb936b588592f
jak7b <olackovic82@gmail.com>
2025-07-31 20:52:44 +0200

    Added item handles

--- src/main/kotlin/net/kuko/remod/init/ItemInit.kt
+++ src/main/kotlin/net/kuko/remod/init/ItemInit.kt
@@ -2,27 +2,22 @@ package net.kuko.remod.init
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.kuko.remod.ReMod
-import net.kuko.remod.item.CustomBucketItem
-import net.minecraft.item.Item
-import net.minecraft.item.Items
+import net.minecraft.item.Item  
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
 object ItemInit {
 
-    val CUSTOM_BUCKET = item("custom_bucket", CustomBucketItem(
-        FabricItemSettings().maxCount(1).recipeRemainder(Items.BUCKET)
-    )
-    )
 
 
-    fun item(name: String, item: Item): Item = Registry.register(
-        Registries.ITEM,
-        Identifier.of(ReMod.MOD_ID, name), item
-    )
+
+
+    fun item(name: String, item: Item): Item =
+        Registry.register(Registries.ITEM, Identifier.of(ReMod.MOD_ID, name), item)
 
     fun init() {
-        ReMod.LOGGER.info("Items have been initlized and registrated for mod: " + ReMod.MOD_ID)
+        ReMod.LOGGER.info("Items have been initialized and registered for mod: " + ReMod.MOD_ID)
     }
 }
+// soon™
