0d5680334e31cb89839a3b5a2d50ceadca41d1cf
jak7b <olackovic82@gmail.com>
2025-08-04 14:40:43 +0200

    I added something lol

--- src/main/kotlin/net/kuko/remod/ReMod.kt
+++ src/main/kotlin/net/kuko/remod/ReMod.kt
@@ -2,14 +2,18 @@ package net.kuko.remod
 
 import net.fabricmc.api.ModInitializer
 import net.kuko.remod.init.ItemInit
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object ReMod : ModInitializer {
-	const val MOD_ID = "remod"
-	val LOGGER = LoggerFactory.getLogger(ReMod.MOD_ID)
+	const val MOD_ID: String = "remod"
+	val LOGGER: Logger = LoggerFactory.getLogger(ReMod.MOD_ID)
 
 	override fun onInitialize() {
-		LOGGER.info("Hello Fabric world!")
-		ItemInit.init()
+		LOGGER.info("Mod: $MOD_ID, Has been loaded!")
+
+
+
+
 	}
 }
